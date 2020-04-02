a42725ac89393b681006635685e36dd9e723deff
原ひかる <harahikaru@harahikarunoMacBook-Air.local>
2020-04-02 09:37:01 +0900

    finish

---
+++ Resource/Resource.h
@@ -0,0 +1,19 @@
+//
+//  Resource.h
+//  Resource
+//
+//  Created by 原ひかる on 2020/04/02.
+//  Copyright © 2020 原ひかる. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Resource.
+FOUNDATION_EXPORT double ResourceVersionNumber;
+
+//! Project version string for Resource.
+FOUNDATION_EXPORT const unsigned char ResourceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Resource/PublicHeader.h>
+
+
