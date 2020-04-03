21f92070a74c91161e49f9d826850f1c5e3b5406
原ひかる <harahikaru@harahikarunoMacBook-Air.local>
2020-04-03 23:18:16 +0900

    [set] Start

---
+++ Ryohiwaru/Domain/Start/StartViewController.swift
@@ -0,0 +1,9 @@
+//
+//  StartViewController.swift
+//  Ryohiwaru
+//
+//  Created by 原ひかる on 2020/04/03.
+//  Copyright © 2020 原ひかる. All rights reserved.
+//
+
+import Foundation
---
+++ Wireframe/Domain/Start/StartBaseView.swift
@@ -0,0 +1,9 @@
+//
+//  StartBaseView.swift
+//  Wireframe
+//
+//  Created by 原ひかる on 2020/04/03.
+//  Copyright © 2020 原ひかる. All rights reserved.
+//
+
+import Foundation
