acddaef47c529876e2e4b531a4832bfb58597a1e
原ひかる <harahikaru@harahikarunoMacBook-Air.local>
2020-04-04 09:26:28 +0900

    [add] BaseRouter

--- Wireframe/Domain/Start/StartBaseRouter.swift
+++ Wireframe/Domain/Start/StartBaseRouter.swift
@@ -7,3 +7,32 @@
 //
 
 import Foundation
+
+open class StartBaseRouter<Dependency: StartBaseDependency> {
+    
+    public typealias View = Dependency.View
+    public typealias Presenter = Dependency.Presenter
+    
+    unowned let view: View
+    
+    public required init(view: View) {
+        self.view = view
+    }
+    
+    open class func assemble() -> View {
+        let presenter = resolvePresenter()
+        let view = View.instantiate(presenter: presenter)
+        let router = self.init(view: view)
+        
+        presenter.view = view
+        presenter.router = router
+        
+        return view
+    }
+    
+    open class func resolvePresenter() -> Presenter {
+        fatalError("Must override resolvePresenter")
+    }
+}
+
+extension StartBaseRouter: StartRouterInterface { }
