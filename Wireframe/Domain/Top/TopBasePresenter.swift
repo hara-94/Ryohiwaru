bb976dc56a24f9b65d1e10802c946a6dbf10e0a8
原ひかる <harahikaru@harahikarunoMacBook-Air.local>
2020-04-04 09:34:23 +0900

    [add] BasePresenter

--- Wireframe/Domain/Top/TopBasePresenter.swift
+++ Wireframe/Domain/Top/TopBasePresenter.swift
@@ -10,12 +10,13 @@ import Foundation
 import Base
 
 open class TopBasePresenter<Dependency: TopBaseDependency>: BasePresenter, TopPresenterContract {
-    public required override init() {
-        super.init()  
-    }
     
     public weak var view: TopViewInterface!
     public var router: TopRouterInterface!
+    
+    public required override init() {
+        super.init()  
+    }
     
     public func update(viewModel: Dependency.ViewModel) {
         if let view = view as? AnyViewRepresentable {
