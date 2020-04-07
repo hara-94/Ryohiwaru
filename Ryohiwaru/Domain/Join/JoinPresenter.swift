8b1674202508b866191c5d715b6867d1b3ee52e0
原暉 <harahikaru@haraakiranoMacBook-Pro.local>
2020-04-07 13:23:26 +0900

    [add]Ryohiwaru/Domain/Join/

---
+++ Ryohiwaru/Domain/Join/JoinPresenter.swift
@@ -0,0 +1,21 @@
+//
+//  JoinPresenter.swift
+//  Ryohiwaru
+//
+//  Created by 原暉 on 2020/04/07.
+//  Copyright © 2020 原ひかる. All rights reserved.
+//
+
+import Foundation
+import Wireframe
+
+final class JoinPresenter: JoinBasePresenter<JoinDependency> {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func on(event: JoinPresenterOperation) {
+        
+    }
+}
---
+++ Ryohiwaru/Domain/Join/JoinViewController.swift
@@ -0,0 +1,21 @@
+//
+//  JoinViewController.swift
+//  Ryohiwaru
+//
+//  Created by 原暉 on 2020/04/07.
+//  Copyright © 2020 原ひかる. All rights reserved.
+//
+
+import Foundation
+import Wireframe
+
+final class JoinViewController: JoinBaseView<JoinDependency> {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func update(viewModel: JoinViewModel?) {
+        super.update(viewModel: viewModel)
+    }
+}
