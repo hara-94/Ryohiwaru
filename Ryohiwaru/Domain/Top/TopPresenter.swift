2512c6bb96fda5d966fd40b32196526e2648ecb5
原暉 <harahikaru@haraakiranoMacBook-Pro.local>
2020-04-07 10:30:26 +0900

    [add]manageへの画面遷移

--- Ryohiwaru/Domain/Top/TopPresenter.swift
+++ Ryohiwaru/Domain/Top/TopPresenter.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2020 原ひかる. All rights reserved.
 //
 
-import Foundation
+import UIKit
 import Base
 import Wireframe
 
@@ -19,7 +19,12 @@ final class TopPresenter: TopBasePresenter<TopDependency> {
     override func on(event: TopPresenterOperation) {
         switch event {
         case .onTapManageButton:
-            print("onTapManageButton")
+            let viewController = ManageRouter.assemble()
+            let navigationController = UINavigationController(rootViewController: viewController)
+            if let view = self.view as? TopViewController {
+                navigationController.modalPresentationStyle = .fullScreen
+                view.present(navigationController, animated: true, completion: nil)
+            }
         case .onTapJoinButton:
             print("onTapJoinButton")
         case .onTapStartButton:
