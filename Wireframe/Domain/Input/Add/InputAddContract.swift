dea74c0cf00f6c9e5d082cfef13d219e30728b0a
原暉 <harahikaru@haraakiranoMacBook-Pro.local>
2020-04-11 08:42:24 +0900

    [add] Wireframe/Domain/Input/Add/

--- Wireframe/Domain/Input/Add/InputAddContract.swift
+++ Wireframe/Domain/Input/Add/InputAddContract.swift
@@ -7,3 +7,30 @@
 //
 
 import Foundation
+import Base
+
+public protocol InputAddBaseDependency: AnyDependency where View: InputAddViewContract {
+    associatedtype Presenter: InputAddPresenterContract
+    associatedtype ViewModel
+    associatedtype PresenterOperation
+}
+
+public protocol InputAddViewInterface: AnyViewInterface {
+    static func instantiate(presenter: InputAddPresenterInterface) -> Self
+}
+
+public protocol InputAddViewContract: InputAddViewInterface {
+    var presenter: InputAddPresenterInterface! { get set }
+}
+
+public protocol InputAddRouterInterface: AnyRouterInterface { }
+
+public protocol InputAddPresenterInterface: AnyPresenterInterface { }
+
+public protocol InputAddPresenterContract: InputAddPresenterInterface {
+    associatedtype Dependency: InputAddBaseDependency
+    
+    var view: InputAddViewInterface! { get set }
+    var router: InputAddRouterInterface! { get set }
+    init()
+}
