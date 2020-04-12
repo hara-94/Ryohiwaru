db3b4b052ffb10364e10db72cd3b98ea244c9538
原暉 <harahikaru@haraakiranoMacBook-Pro.local>
2020-04-12 21:02:35 +0900

    [add] Wireframe/Domain/Calendar

---
+++ Wireframe/Domain/Calendar/CalendarBaseRouter.swift
@@ -0,0 +1,36 @@
+//
+//  CalendarBaseRouter.swift
+//  Wireframe
+//
+//  Created by 原暉 on 2020/04/12.
+//  Copyright © 2020 原ひかる. All rights reserved.
+//
+
+import Foundation
+
+open class CalendarBaseRouter<Depedency: CalendarBaseDependency> {
+    public typealias View = Depedency.View
+    public typealias Presenter = Depedency.Presenter
+    
+    unowned let view: View
+    
+    public required init(view: View) {
+        self.view = view
+    }
+    
+    open class func assemble() -> View {
+        let presenter = resolvePresenter()
+        let view = View.instantiate(presenter: presenter)
+        let router = self.init(view: view)
+        
+        presenter.view = view
+        presenter.router = router
+        return view
+    }
+    
+    open class func resolvePresenter() -> Presenter {
+        fatalError("Must override resolvePresenter")
+    }
+}
+
+extension CalendarBaseRouter: CalendarRouterInterface { }
